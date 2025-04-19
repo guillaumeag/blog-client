ef9e38ee8ee0c4f40b20f1c7e667b18f81bf4590
Guillaume G. <guillaumeag@users.noreply.github.com>
2025-04-19 13:02:38 +0200

    Home: Fetch dummy blog posts from Vercel API and list the titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import Image from "next/image";
 
-export default function Home() {
+// Define the structure of a blog post object
+// in order to use it in the page
+type Post = {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  date: Date;
+  category: string;
+}
+
+export default async function Home() {
+  
+  // Fetching data from an API
+  const blog_data = await fetch("https://api.vercel.app/blog");
+  
+  // Process the response and parse data as JSON
+  const posts: Post[] = await blog_data.json();
+  
   return (
     <div>
       <main>
         <h1 className="text-4xl font-bold">Next Blog</h1>
+
+        <ul>
+          {posts.map((post: Post) => (<li key={post.id}>{post.title}</li>))}
+        </ul>
       </main>
     </div>
   );
